a3036cc97007a74cdf3afb3d794559205882e1ec
Carlos Mestre <carlosg@opera.com>
2011-05-07 21:34:12 +0200

    Commando importar datos desde fichero con datos anuales

---
+++ calima/caligo/management/commands/generatedata.py
@@ -0,0 +1,62 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from cliente import Calima
+
+from caligo.models import Station, Province, DailyReport
+
+class Command(BaseCommand):
+    args = '<directorio>'
+    help = 'Importa los datos de un directorio a la db'
+
+    def handle(self, *args, **options):
+        calima = Calima(path=args[0])
+        calima.generarDatosAnual(2009)
+        for estacion in calima.estaciones.values():
+            province, created = Province.objects.get_or_create(
+                    name=unicode(estacion.provincia,
+                        'iso8859-1').encode('utf8'))
+            
+            if created:
+                print "Created province %s" % province.name
+
+            print "Processing estacion", estacion.altitud, estacion.latitud, estacion.longitud, estacion.id, estacion.nombre
+            station, created = Station.objects.get_or_create(code=estacion.id,
+                    name=unicode(estacion.nombre, 'iso8859-1').encode('utf8'),
+                    province=province,
+                    altitude=estacion.altitud,
+                    latitude=estacion.latitud,
+                    longitude=estacion.longitud)
+            if created:
+                print "created station %s" % station.name
+
+            for key in estacion.valores:
+                datos = estacion.valores[key]
+             #   for dat in datos:
+             #       print dat, datos[dat]
+             #   print "======="
+                daily = DailyReport(date=key,
+                        station=station,
+                        max_t          = datos['t_max'][0],
+                        max_t_time     = datos['t_max'][1],
+                        min_t          = datos['t_min'][0],
+                        min_t_time     = datos['t_min'][1],
+                        avg_t          = datos['t_med'],
+                        squall         = datos['racha'][0],
+                        squall_dir     = datos['racha'][1],
+                        squall_time    = datos['racha'][2],
+                        wind_avg_speed = datos['vel_media'],
+                        # yeah, I know
+                        precip         = type(datos['prec']) != str and datos['prec'] or None,
+                        sunshine       = datos['sol'],
+                        max_press      = datos['max_press'][0],
+                        max_press_time = datos['max_press'][1],
+                        min_press      = datos['min_press'][0],
+                        min_press_time = datos['min_press'][1],
+                        imp_prec      = datos['prec'] == 'Ip' and True or False,
+                )
+                daily.save()
+                #TODO
+#                        impc_prec      = datos[''],
+#                        var_w_dir      = datos['t_med'])
+
+
