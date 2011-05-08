57835b1d5beaa45579ddbf5d6585854737b3ae6f
Carlos Mestre <carlosg@opera.com>
2011-05-08 13:59:10 +0200

    provinces/station views, empty templates, 404.html, urls...

--- calima/caligo/urls.py
+++ calima/caligo/urls.py
@@ -4,4 +4,7 @@ from caligo import views
 urlpatterns = patterns('',
     url(r'^provinces/(?P<provinceId>\d+)', views.provinces),
     url(r'^provinces/', views.provinces),
+
+    url(r'^stations/(?P<stationId>\w+)', views.stations),
+    url(r'^stations/', views.stations),
 )
