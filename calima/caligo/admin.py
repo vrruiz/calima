98a437b3d0299ac5433dd113142ed5a984761f73
Carlos Mestre <carlosg@opera.com>
2011-05-08 11:34:03 +0200

    INSTALL update, uso correcto de get_or_create, mejora de admin para saber los datos importados

--- calima/caligo/admin.py
+++ calima/caligo/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from caligo.models import Station, Province, DailyReport
 
 
-admin.site.register(Station)
 admin.site.register(Province)
 class DailyReportAdmin(admin.ModelAdmin):
     list_display = ('station', 'date')
+    list_filter = ['date', 'station', ]
 
-    list_filter = ['date']
+class StationAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'province')
+    list_filter = ['province',]
 
+admin.site.register(Station, StationAdmin)
 admin.site.register(DailyReport, DailyReportAdmin)
--- calima/caligo/models.py
+++ calima/caligo/models.py
@@ -85,37 +85,38 @@ def station_importer(estaciones):
 
         print "Processing estacion", estacion.altitud, estacion.latitud, estacion.longitud, estacion.id, estacion.nombre
         station, created = Station.objects.get_or_create(code=estacion.id,
-                name=unicode(estacion.nombre, 'iso8859-1').encode('utf8'),
-                province=province,
-                altitude=estacion.altitud,
-                latitude=estacion.latitud,
-                longitude=estacion.longitud)
+                defaults={
+                'name'      : unicode(estacion.nombre, 'iso8859-1').encode('utf8'),
+                'province'  : province,
+                'altitude'  : estacion.altitud,
+                'latitude'  : estacion.latitud,
+                'longitude' : estacion.longitud})
         if created:
-            print "created station %s" % station.name
+            print "Created station %s" % station.name
 
         for key in estacion.valores:
             datos = estacion.valores[key]
-            daily = DailyReport(date=key,
-                    station=station,
-                    max_t          = datos['t_max'][0],
-                    max_t_time     = datos['t_max'][1],
-                    min_t          = datos['t_min'][0],
-                    min_t_time     = datos['t_min'][1],
-                    avg_t          = datos['t_med'],
-                    squall         = datos['racha'][0],
-                    squall_dir     = datos['racha'][1],
-                    squall_time    = datos['racha'][2],
-                    wind_avg_speed = datos['vel_media'],
-                    # yeah, I know
-                    precip         = type(datos['prec']) != str and datos['prec'] or None,
-                    sunshine       = datos['sol'],
-                    max_press      = datos['max_press'][0],
-                    max_press_time = datos['max_press'][1],
-                    min_press      = datos['min_press'][0],
-                    min_press_time = datos['min_press'][1],
-                    imp_prec      = datos['prec'] == 'Ip' and True or False,
+            obj, created = DailyReport.objects.get_or_create(
+                date=key, station=station, defaults={
+                    'max_t'          : datos['t_max'][0],
+                    'max_t_time'     : datos['t_max'][1],
+                    'min_t'          : datos['t_min'][0],
+                    'min_t_time'     : datos['t_min'][1],
+                    'avg_t'          : datos['t_med'],
+                    'squall'         : datos['racha'][0],
+                    'squall_dir'     : datos['racha'][1],
+                    'squall_time'    : datos['racha'][2],
+                    'wind_avg_speed' : datos['vel_media'],
+                    'precip'         : type(datos['prec']) != str and datos['prec'] or None,
+                    'sunshine'       : datos['sol'],
+                    'max_press'      : datos['max_press'][0],
+                    'max_press_time' : datos['max_press'][1],
+                    'min_press'      : datos['min_press'][0],
+                    'min_press_time' : datos['min_press'][1],
+                    'imp_prec'       : datos['prec'] == 'Ip' and True or False,
+                }
             )
-            daily.save()
+
             # TODO
             # si el viento es 99 no he hecho nada.. habra que actualizar
             # campo
