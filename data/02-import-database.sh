7cc17c2e580dc484d471f55f7d7c4a36c1d73d6e
Víctor R. Ruiz <vrruiz@gmail.com>
2011-05-13 16:07:23 +0100

    Imprimir año en la importación de la BBDD

--- data/02-import-database.sh
+++ data/02-import-database.sh
@@ -4,6 +4,7 @@ YEARS=`ls ../data/datos/*.CSV.gz`
 for y in $YEARS
 do
     year=`echo $y | sed -e "s/..\/data\/datos\/\(.*\).CSV.gz/\1/"`
+    print $year
     python manage.py importer ../data/datos $year
 done
 cd -
