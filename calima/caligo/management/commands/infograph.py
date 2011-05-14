a449bc2867ac4e91fe6116e645cfdab6ea4a7133
Víctor R. Ruiz <vrruiz@gmail.com>
2011-05-14 16:06:37 +0100

    Añadido el infográfico de promedios anuales (todas las estaciones)

--- calima/caligo/management/commands/infograph.py
+++ calima/caligo/management/commands/infograph.py
@@ -2,7 +2,7 @@
 ##
 ## calima - Genera un gráfico de información con el histórico de datos
 ##
-## $ python manage.py infograph <fichero.png> --year <año> | --station <estación>
+## $ python manage.py infographic <fichero.png>
 ##
 import calendar
 
@@ -14,12 +14,15 @@ from django.db.models import Avg, Max, Min, Count
 from caligo.models import DailyReport, Station
 from color_scheme import scheme
 
+from django.db import connection
+
 PIX_SIZE = 25
 TEMP_MIN = -15
 TEMP_MAX = 45
 COLOR_WEIGHT = 256.0 / (TEMP_MAX + abs(TEMP_MIN) + 1)
 FONT = '/Library/Fonts/Arial.ttf' # TODO: Find font path
 FONT_SIZE = 20
+FONT_SIZE_SMALL = 10
 MARGIN_FONT = 10
 LOGO_IMAGE='/Users/rvr/archivos/prog/abredatos/calima/calima/caligo/static/images/calima-300x76.png' # TODO: Relative path
 
@@ -31,22 +34,21 @@ MONTHS = [
 
 class InfoGraphic():
     """ Creates an infographic image """
-    
+
     def __init__(self, image_filename):
         """ Constructor """
         self.filename = image_filename
-        
+
     def generate_by_year(self, stations, daily_reports, year):
         """ Generates an infographic image showing temperature """
 
         def station_title(station):
-            """ Returns the latitude and name of the station """ 
+            """ Returns the latitude and name of the station """
             hemisphere = 'N'
             if (station.latitude < 0):
                 hemisphere = 'S'
             return "%s (%+1.0f%s, %i m)" % (station.name, station.latitude, hemisphere, station.altitude)
 
-                    
         # Number of stations
         stations_num = stations.count()
         station_pos = dict()
@@ -59,7 +61,7 @@ class InfoGraphic():
             font_size = font.getsize(station_title(station))
             if (font_size[0] > margin_x):
                 margin_x = font_size[0]
-            station_pos[station.name] = pos_y
+            station_pos[station.code] = pos_y
             pos_y = pos_y + PIX_SIZE
         # Create image
         margin_x = margin_x + MARGIN_FONT
@@ -78,17 +80,17 @@ class InfoGraphic():
         # Draw station names
         for station in stations:
             # Draw background
-            color = (230,230,230) 
+            color = (230,230,230)
             if (background_switch == False):
-           	    color = "White"
+                color = "White"
             background_switch = not background_switch
-            p_y = margin_y + station_pos[station.name]
+            p_y = margin_y + station_pos[station.code]
             p_x = 0
             draw.rectangle([p_x, p_y, margin_x - 1, p_y + PIX_SIZE - 1], fill=color)
             # Draw text
             font_size = font.getsize(station_title(station))
             p_x = margin_x - font_size[0] - int(MARGIN_FONT / 2)
-            p_y = margin_y + station_pos[station.name] + int((PIX_SIZE - font_size[1]) / 2)
+            p_y = margin_y + station_pos[station.code] + int((PIX_SIZE - font_size[1]) / 2)
             draw.text((p_x, p_y), station_title(station), fill="Black")
         # Draw temperature palette
         i = 0
@@ -108,14 +110,14 @@ class InfoGraphic():
         for report in daily_reports:
             day_of_year = int(report.date.strftime('%j'))
             p_x = margin_x + (day_of_year - 1) * PIX_SIZE
-            p_y = margin_y + station_pos[report.station.name]
+            p_y = margin_y + station_pos[report.station.code]
             # Calculate color palette index
             color_index = 255 - int(float(report.avg_t + abs(TEMP_MIN)) * COLOR_WEIGHT)
             if color_index < 0:
                 color_index = 0
             elif color_index > 255:
                 color_index = 255
-            color = scheme[color_index] 
+            color = scheme[color_index]
             # Draw rectangle
             draw.rectangle([p_x, p_y, p_x + PIX_SIZE - 1, p_y + PIX_SIZE - 1], fill=color)
         # Draw months
@@ -132,7 +134,7 @@ class InfoGraphic():
             days = days + MONTHS[month][1]
             # Leap year, add a day to the calendar
             if (month == 2 and year_is_leap):
-            	days = days + 1
+                days = days + 1
             # Draw month limit (finish)
             if (month < 11):
                 p_x = margin_x + PIX_SIZE * days
@@ -148,7 +150,7 @@ class InfoGraphic():
             im.paste(logo, (p_x, p_y, p_x + logo.size[0], p_y + logo.size[1]))
         del draw
         im.save(self.filename, "PNG")
-        
+
     def image_by_year(self, year):
         """ Generate infographic by year """
         if (year < 1920 or year > 2050):
@@ -158,6 +160,127 @@ class InfoGraphic():
         # Daily reports, ordered by date
         daily_reports = DailyReport.objects.filter(date__year=year).order_by('date').filter(avg_t__isnull=False)
         self.generate_by_year(stations, daily_reports, year)
+
+    def generate_by_average(self, stations, year_min, year_max):
+        """ Generates an infographic image showing temperature """
+
+        def station_title(station):
+            """ Returns the latitude and name of the station """
+            hemisphere = 'N'
+            if (station.latitude < 0):
+                hemisphere = 'S'
+            return "%s (%+1.0f%s, %i m)" % (station.name, station.latitude, hemisphere, station.altitude)
+
+        # Number of years
+        years_num = year_max - year_min + 1
+        # Number of stations
+        stations_num = stations.count()
+        station_pos = dict()
+        # Generate a dictionary with stations' row positions
+        margin_x = 0
+        margin_y = PIX_SIZE * 2
+        pos_y = 0
+        font = ImageFont.truetype(FONT, FONT_SIZE)
+        for station in stations:
+            font_size = font.getsize(station_title(station))
+            if (font_size[0] > margin_x):
+                margin_x = font_size[0]
+            station_pos[station.code] = pos_y
+            pos_y = pos_y + PIX_SIZE
+        margin_x = margin_x + PIX_SIZE
+        # Create image
+        margin_x = margin_x + MARGIN_FONT
+        size_x = margin_x + (years_num + 1) * PIX_SIZE
+        size_y = (stations_num + 3) * PIX_SIZE + margin_y
+        im = Image.new("RGB", (size_x, size_y), "White")
+        draw = ImageDraw.Draw(im)
+        draw.setfont(font)
+        # Draw title
+        title = "Mapa de temperaturas anuales promedio - Datos AEMET - calima.linotipo.es"
+        font_size = font.getsize(title)
+        p_x = (size_x - font_size[0]) / 2
+        p_y = 0
+        draw.text((p_x, p_y), title, fill="Black")
+        # Draw years
+        for year in range(year_min, year_max + 1, 5):
+            font_size = font.getsize(str(year))
+            p_x = margin_x + (year - year_min) * PIX_SIZE
+            p_y = PIX_SIZE
+            draw.text((p_x, p_y), str(year), fill="Black")
+        # Draw station names
+        background_switch = True
+        for station in stations:
+            # Draw background
+            color = (240,240,240)
+            if (background_switch == False):
+                color = "White"
+            background_switch = not background_switch
+            p_y = margin_y + station_pos[station.code]
+            p_x = PIX_SIZE
+            # draw.rectangle([p_x, p_y, margin_x - 1, p_y + PIX_SIZE - 1], fill=color)
+            draw.rectangle([p_x, p_y, size_x - PIX_SIZE, p_y + PIX_SIZE - 1], fill=color)
+            # Draw text
+            font_size = font.getsize(station_title(station))
+            p_x = margin_x - font_size[0] - int(MARGIN_FONT / 2)
+            p_y = margin_y + station_pos[station.code] + int((PIX_SIZE - font_size[1]) / 2)
+            draw.text((p_x, p_y), station_title(station), fill="Black")
+       # Draw temperature palette
+        i = 0
+        for temperature in range(TEMP_MIN, TEMP_MAX + 1):
+            p_x = margin_x + i * PIX_SIZE
+            p_y = margin_y + (stations_num + 1) * PIX_SIZE
+            color_index = 255 - int(float(temperature + abs(TEMP_MIN)) * COLOR_WEIGHT)
+            color = scheme[color_index]
+            # Draw rectangle
+            draw.rectangle([p_x, p_y, p_x + PIX_SIZE - 1, p_y + PIX_SIZE - 1], fill=color)
+            # Draw text
+            font_size = font.getsize(str(temperature))
+            p_x = p_x + int((PIX_SIZE - font_size[0]) / 2)
+            draw.text((p_x, p_y), str(temperature), fill="White")
+            i = i + 1
+        # Draw temperatures
+        for station in stations:
+            print station
+            for year in range(year_min, year_max + 1):
+                count = DailyReport.objects.filter(station=station, date__year=year, avg_t__isnull=False).aggregate(count=Count('avg_t'))['count']
+                if (count < 330):
+                    continue
+                avg = DailyReport.objects.filter(station=station, date__year=year, avg_t__isnull=False).aggregate(avg=Avg('avg_t'))['avg']
+                if (not avg):
+                    print 'ERROR: %s %i %d (%s)' % (station.name, year, count, connection.queries[-1])
+                    continue
+                average = float(avg)
+                p_x = margin_x + (year - year_min) * PIX_SIZE
+                p_y = margin_y + station_pos[station.code]
+                # Calculate color palette index
+                color_index = 255 - int(float(average + abs(TEMP_MIN)) * COLOR_WEIGHT)
+                if color_index < 0:
+                    color_index = 0
+                elif color_index > 255:
+                    color_index = 255
+                color = scheme[color_index]
+                # Draw rectangle
+                draw.rectangle([p_x, p_y, p_x + PIX_SIZE - 1, p_y + PIX_SIZE - 1], fill=color)
+        # Draw logo
+        if (LOGO_IMAGE):
+            logo = Image.open(LOGO_IMAGE)
+            logo_size = 200
+            logo.thumbnail((logo_size, logo_size), Image.NEAREST or Image.ANTIALIAS)
+            p_x = size_x - logo.size[0]
+            p_y = size_y - logo.size[1]
+            im.paste(logo, (p_x, p_y, p_x + logo.size[0], p_y + logo.size[1]))
+        del draw
+        im.save(self.filename, "PNG")
+
+    def image_by_average(self):
+        """ Generate infographic by year """
+        # Stations, ordered by latitude and longitude
+        stations = Station.objects.all().order_by('-latitude', 'latitude')
+        # Daily reports, ordered by date
+        # daily_reports = DailyReport.objects.order_by('station', 'date').filter(avg_t__isnull=False)
+        year_min = DailyReport.objects.aggregate(date_min=Min('date'))['date_min'].year
+        year_max = DailyReport.objects.aggregate(date_max=Max('date'))['date_max'].year
+        self.generate_by_average(stations, year_min, year_max)
 
     def generate_by_station(self, daily_reports, station, year_min, year_max):
         """ Generates an infographic image showing temperature """
@@ -193,9 +316,9 @@ class InfoGraphic():
         for year in year_pos.keys():
             """
             # Draw background
-            color = (230,230,230) 
+            color = (230,230,230)
             if (background_switch == False):
-           	    color = "White"
+                color = "White"
             background_switch = not background_switch
             p_y = margin_y + year_pos[year]
             p_x = 0
@@ -284,6 +407,7 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
             make_option('--year', '-y', action='store_true', dest='year'),
             make_option('--station', '-s', action='store_true', dest='station'),
+            make_option('--average', '-a', action='store_true', dest='average'),
         )
 
     def handle(self, *args, **options):
@@ -297,4 +421,9 @@ class Command(BaseCommand):
             station = args[1]
             graph = InfoGraphic(image)
             graph.image_by_station(station)
+        elif (options.get('average')):
+            image = args[0]
+            graph = InfoGraphic(image)
+            graph.image_by_average()
+
 
